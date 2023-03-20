7a491ba7fab06f4f916173fa280398883ea1c5a7
Gladston de Paula <gladston.dev@gmail.com>
2023-03-20 12:38:43 -0300

    Organização de arquivos

--- Estudo Geral/3-6-classes_arquivos/js-final/app.js
+++
@@ -1,9 +0,0 @@
-$('.abrir-modal').click(function(){
-	$('.modal').toggleClass('active');
-	var condicao = $('.modal').hasClass('active');
-	if(condicao) {
-		$(this).text('Fechar Modal');
-	} else {
-		$(this).text('Abrir Modal');
-	}
-});
